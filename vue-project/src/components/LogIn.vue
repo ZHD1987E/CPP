fd184f18d697b7cfbd5b7e376fd389d32de89779
zhd1987e <zhanghaodong101@outlook.com>
2024-10-06 22:02:01 +0800

    Implemented Login

--- vue-project/src/components/LogIn.vue
+++ vue-project/src/components/LogIn.vue
@@ -1,14 +1,14 @@
 <script setup>
 import {ref} from "vue";
 import {auth} from "../firebase.js"
-import { createUserWithEmailAndPassword } from "firebase/auth"
+import { signInWithEmailAndPassword } from "firebase/auth"
 
 const email = ref('')
 const password = ref('')
 const signupmessage = ref('')
 
 async function signup() {
-    createUserWithEmailAndPassword(auth, email.value, password.value).then((usercredential) => {
+    signInWithEmailAndPassword(auth, email.value, password.value).then((usercredential) => {
         signupmessage.value = "Congratulations!"
         console.log(usercredential.user)
         email.value = ""
@@ -22,9 +22,9 @@ async function signup() {
 </script>
 <template>
 <form @submit.prevent="signup">
-    <label id="email">Enter your Email</label>
+    <label id="email">Enter your Email to login</label>
     <input v-model="email" id="email" required>
-    <label id="pwd">Enter your Password</label>
+    <label id="pwd">Enter your Password to login</label>
     <input v-model="password" id="pwd" required>
     <input type="submit">
 </form>
--- vue-project/src/views/TestLogin.vue
+++ vue-project/src/views/TestLogin.vue
@@ -1,9 +1,11 @@
 <script setup>
 import SignUp from "../components/SignUp.vue"
+import LogIn from "../components/LogIn.vue"
 </script>
 
 <template>
 <SignUp></SignUp>
+<LogIn></LogIn>
 </template>
 
 <style scoped>
