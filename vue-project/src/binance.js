3033f6ef5e169f707a4e1d131e153bfaa37c0db0
zhd1987e <zhanghaodong101@outlook.com>
2024-10-06 22:19:17 +0800

    Exported stuff out

--- vue-project/src/binance.js
+++
@@ -1,5 +0,0 @@
-import { binance } from "ccxt"
-
-const exchange = new binance();
-
-export { exchange }
--- vue-project/src/main.js
+++
@@ -1,5 +0,0 @@
-import router from "./router/index"
-import { createApp } from 'vue'
-import App from './App.vue'
-
-createApp(App).use(router).mount('#app')
